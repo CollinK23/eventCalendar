b92faf087f65f5a9b12715e29f32ce8fd80eb801
CollinK23 <kimballcollin@gmail.com>
2023-01-11 18:33:42 -0800

    db fix

--- eventcal/settings.py
+++ eventcal/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = SECRET_KEY='kyp8*komn5bq+22e5djwmgdkxtbecw*wh5t!1$*ot9#v&5*e$^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
